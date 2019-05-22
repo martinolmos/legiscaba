a0befdaff4229d68dd7ed2820d2a0e931296d5d9
Martin Olmos <martinolmos@gwmail.gwu.edu>
2019-05-22 12:05:24 -0300

    Función getExpediente

--- R/expedientes.R
+++ R/expedientes.R
@@ -1,8 +1,15 @@
 #' Función que hace un requerimiento de expedientes
 #'
-#'
-
-ua <- httr::user_agent("http://github.com/martinolmos/legiscaba")
+#' @param IdProyectoTipo integer, ID del tipo de proyecto. Ver getTiposProyecto
+#' @param IdAutoresInternos integer, ID de el/la autor/a # debería haber un lugar donde buscar el id por nombre
+#' @param IdUbicacion integer, ID de la ubicación
+#' @param IdEstado integer, ID del Estado
+#' @param Sumario character, Texto a buscar en el sumario
+#' @param SumarioExacto integer, Macheo exacto: 0 para Falso y 1 para Verdadero
+#' @param FechaDesde character, Acotar búsqueda desde fecha. Formato dd/mm/aaaa
+#' @param FechaHasta character, Acotar búqueda hasta fecha. Formato dd/mm/aaaa
+#' @param AnioParlamentario integer, Acotar la búsqueda a un año. Formato aaaa
+#' @param Limite integer, Limitar la respuesta a un número de registros
 
 getExpediente <- function(IdProyectoTipo = "", IdAutoresInternos = "",
                           IdUbicacion = "", IdEstado = "", Sumario = "",
@@ -20,6 +27,8 @@ getExpediente <- function(IdProyectoTipo = "", IdAutoresInternos = "",
 
     myurl <- httr::modify_url(url = base_url, path = path, query = query)
 
+    ua <- httr::user_agent("https://github.com/martinolmos/legiscaba")
+
     resp <- httr::GET(url = myurl, ua)
 
         if (httr::http_type(resp) != "text/xml") {
@@ -28,9 +37,13 @@ getExpediente <- function(IdProyectoTipo = "", IdAutoresInternos = "",
 
     parsed <- httr::content(resp)
 
-    if (httr::http_error(resp)) {
-        stop(sprintf("Falló el requerimiento a la API [%s]\n%s\n<%s>", httr::status_code(resp),
-                     parsed$message), call. = FALSE)
+    if (httr::status_code(resp) != 200) {
+        stop(
+            sprintf(
+                "Falló el requerimiento a la API [%s]\n%s",
+                httr::status_code(resp),
+                parsed$message),
+            call. = FALSE)
     }
 
     structure(list(content = parsed, path = path, response = resp), class = "expediente")
@@ -40,5 +53,31 @@ print.expediente <- function(x, ...) {
     cat("<Parlamentaria ", x$path, "\n", sep = "")
     str(x$content)
     invisible(x)
+
+}
+
+#' Función que parsea un expediente
+#'
+#' @param Expediente expediente, Ver getExpediente
+
+expedienteToDF <- function(Expediente = expediente) {
+    xml2::xml_ns_strip(expediente$content)
+
+    rows <- expediente$content %>%
+        xml2::xml_find_all("//expedienteAvanzado") %>%
+        purrr::map(~ xml2::xml_find_all(., "*"))
+
+    rows_df <- dplyr::tibble(row = seq_along(rows),
+                             nodeset = rows)
+
+    rows_df %>%
+        dplyr::mutate(col_name_raw = nodeset %>%
+                          purrr::map(~ xml2::xml_name(.)),
+                      cell_text = nodeset %>%
+                          purrr::map(~ xml2::xml_text(.)),
+                      i = nodeset %>%
+                          purrr::map(~ seq_along(.))) %>%
+        select(row, i, col_name_raw, cell_text) %>%
+        tidyr::unnest()
 
 }

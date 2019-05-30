33abc5c5a6c251454f7d3fa3ff0b35a589032061
Martin Olmos <martinolmos@gwmail.gwu.edu>
2019-05-30 15:48:12 -0300

    getExpedienteGiros

--- R/expedientes.R
+++ R/expedientes.R
@@ -50,9 +50,9 @@ getExpediente <- function(IdProyectoTipo = "", IdAutoresInternos = "",
 }
 
 
-#' Función que parsea un expediente
+#' Función que parsea un expediente de XML a tibble
 #'
-#' @param Expediente expediente, Ver getExpediente
+#' @param Expediente expediente en formato XML
 #' @importFrom dplyr %>%
 
 expedienteToDF <- function(Expediente = expediente) {
@@ -60,6 +60,69 @@ expedienteToDF <- function(Expediente = expediente) {
 
     rows <- Expediente %>%
         xml2::xml_find_all("//expedienteAvanzado") %>%
+        purrr::map(~ xml2::xml_find_all(., "*"))
+
+    rows_df <- dplyr::tibble(row = seq_along(rows),
+                             nodeset = rows)
+
+    rows_df %>%
+        dplyr::mutate(col_name_raw = nodeset %>%
+                          purrr::map(~ xml2::xml_name(.)),
+                      cell_text = nodeset %>%
+                          purrr::map(~ xml2::xml_text(.)),
+                      i = nodeset %>%
+                          purrr::map(~ seq_along(.))) %>%
+        dplyr::select(row, i, col_name_raw, cell_text) %>%
+        tidyr::unnest() %>%
+        dplyr::select(-i) %>%
+        tidyr::spread(key = col_name_raw, value = cell_text) %>%
+        dplyr::select(-row)
+
+}
+
+#' Función para requerir los giros de un expediente
+#'
+#' @param IdExpediente Integer Número de ID del expediente
+#' @export
+
+getExpedienteGiros <- function(IdExpediente) {
+    base_url <- "https://parlamentaria.legislatura.gov.ar"
+
+    path <- "webservices/Json.asmx/GetExpedienteGiros"
+
+    query <- list(IdExpediente = IdExpediente)
+
+    myurl <- httr::modify_url(url = base_url, path = path, query = query)
+
+    ua <- httr::user_agent("https://github.com/martinolmos/legiscaba")
+
+    resp <- httr::GET(url = myurl, ua)
+
+    if (httr::status_code(resp) != 200) {
+        stop(
+            sprintf(
+                "Falló el requerimiento a la API [%s]\n%s",
+                httr::status_code(resp)),
+            call. = FALSE)
+    }
+
+    if (httr::http_type(resp) != "text/xml") {
+        stop("la API no retornó un xml", call. = FALSE)
+    }
+
+    parsed <- httr::content(resp)
+    giroToDF(parsed)
+
+}
+
+#' Función que parsea un giro de XML a tibble
+#'
+#' @param Giro giro en formato XML
+giroToDF <- function(Giro) {
+    xml2::xml_ns_strip(Giro)
+
+    rows <- Giro %>%
+        xml2::xml_find_all("//expedienteGiros") %>%
         purrr::map(~ xml2::xml_find_all(., "*"))
 
     rows_df <- dplyr::tibble(row = seq_along(rows),
